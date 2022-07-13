c4ec4c1cdc7e905cfdb762e7ad14ba1b91c05ee9
RZ Code Studio <73451384+RZ-Code-Studio@users.noreply.github.com>
2022-07-12 18:57:36 -0700

    Update jDash.js

--- jDash/jDash.js
+++ jDash/jDash.js
@@ -1,24 +1,30 @@
-(function(){
-	class JDash {
-		constructor (cssTag) {
-			this.selected = document.querySelectorAll(cssTag);
-			this.textContents = [];
-			
-			for (let i = 0; i < this.selected.length; i++) {
-				this.textContents.push(this.selected[i].textContent);
-			}
-		}
-		
-		text (newText) {
-			if (newText === undefined) {
-				return this.textContents;
-			} else {
-				for (let i = 0; i < this.textContents.length; i++) {
-					this.textContents[i] = newText;
-				}
-				
-				return this;
-			}
-		}
-	}
-})()
+class JDash {
+    constructor(cssTag) {
+        this.selected = document.querySelectorAll(cssTag);
+        this.textContents = [];
+        
+        for (let i = 0; i < this.selected.length; i++) {
+            this.textContents.push(this.selected[i].textContent);
+        }
+    }
+    
+    text(newText) {
+        if (newText === undefined) {
+            return this.textContents;
+        } else {
+            for (let i = 0; i < this.textContents.length; i++) {
+                this.textContents[i] = newText;
+            }
+            
+            return this;
+        }
+    }
+}
+
+function jDash (cssTag) {
+	return new JDash(cssTag);
+}
+
+function $ (cssTag) {
+	return new JDash(cssTag);
+}
