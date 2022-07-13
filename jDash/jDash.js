f39079b1a4b4c3cc21057696fa7ff2af6f02ed80
RZ Code Studio <73451384+RZ-Code-Studio@users.noreply.github.com>
2022-07-12 18:52:32 -0700

    Create jDash.js

---
+++ jDash/jDash.js
@@ -0,0 +1,24 @@
+(function(){
+	class JDash {
+		constructor (cssTag) {
+			this.selected = document.querySelectorAll(cssTag);
+			this.textContents = [];
+			
+			for (let i = 0; i < this.selected.length; i++) {
+				this.textContents.push(this.selected[i].textContent);
+			}
+		}
+		
+		text (newText) {
+			if (newText === undefined) {
+				return this.textContents;
+			} else {
+				for (let i = 0; i < this.textContents.length; i++) {
+					this.textContents[i] = newText;
+				}
+				
+				return this;
+			}
+		}
+	}
+})()
